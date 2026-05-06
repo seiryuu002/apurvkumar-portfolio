55b63115ff4b030b4fc64be8e76ebcd78dbdaf3c
Apurv Kumar <sapurv.skumar250@gmail.com>
2026-05-07 00:12:24 +0530

    feat: add scene

--- flipvilla/src/main.js
+++ flipvilla/src/main.js
@@ -5,13 +5,38 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //scene
 const scene = new Three.Scene();
-scene.background = new Three.Color(0x202025);
+scene.background = new Three.Color(0x87CEEB);
+
+// Ground
+const groundGeometry = new Three.PlaneGeometry(100, 100);
+const groundMaterial = new Three.MeshStandardMaterial({
+  color: 0x228B22
+})
+
+const ground = new Three.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+ground.position.y = -1.5;
+scene.add(ground);
+
+// Loader 
+const loader = new GLTFLoader();
+function loadModel(path, position = [0,0,0], scale = [1,1,1]){
+  loader.load(path, (gltf) => {
+    const model = gltf.scene;
+    model.position.set(...position);
+    model.scale.set(...scale);
+    console.log('Model loaded:', path);
+    scene.add(model);
+  }, undefined, (error) => {
+    console.error('Error loading model:', error);
+  });
+}
 
 
 // Camera 
 const camera = new Three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
-camera.position.set(0,2,5);
+camera.position.set(8,6,10);
+camera.lookAt(0,0,0);
 
 //Renderer
 const renderer = new Three.WebGLRenderer({antialias: true});
@@ -22,42 +47,21 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-// model
-const loader = new GLTFLoader();
-loader.load(
-  '/flipvilla/models/human.glb', 
+// human model
+loadModel('/flipvilla/models/human.glb', [0, -1.5, 0], [1, 1, 1]);
 
-  (gltf) => {
-    const model = gltf.scene;
+loadModel('/flipvilla/models/human.glb', [-5, 0, 3], [1, 1, 1]);
+loadModel('/flipvilla/models/human.glb', [5, 0, -3], [1, 1, 1]);
 
-    scene.add(model);
-
-    // scale fix (VERY common)
-    model.scale.set(1, 1, 1);
-
-    // position fix
-    model.position.set(0, -1.5, 0);
-
-    console.log('Model loaded ✅');
-  },
-
-  (progress) => {
-    console.log((progress.loaded / progress.total) * 100 + '% loaded');
-  },
-
-  (error) => {
-    console.error('Error loading model ❌', error);
-  }
-);
 
 //Light
 const hemisphereLight = new Three.HemisphereLight(0xffffff, 0x444444, 1);
 scene.add(hemisphereLight);
 
 //Directional Light
-const dirLight = new Three.DirectionalLight(0xffffff, 1);
-dirLight.position.set(5, 10, 5);
-scene.add(dirLight);
+const sun = new Three.DirectionalLight(0xffffff, 1.5);
+sun.position.set(10, 20, 10);
+scene.add(sun);
 
 // resize
 window.addEventListener('resize', () => {
